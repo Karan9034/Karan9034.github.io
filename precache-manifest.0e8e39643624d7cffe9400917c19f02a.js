231dce59be7c342678c6240d097e707cde3d1832
Karan9034 <karan.agr9034@gmail.com>
2021-04-02 18:21:48 +0530

    Updates

---
+++ precache-manifest.0e8e39643624d7cffe9400917c19f02a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0140211ccf63a02ead33ce1a87d9e3c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d968922a305d813f843f",
+    "url": "/static/css/2.5de2e9f9.chunk.css"
+  },
+  {
+    "revision": "0387ace5873b0185fe89",
+    "url": "/static/css/main.cfed8ae1.chunk.css"
+  },
+  {
+    "revision": "d968922a305d813f843f",
+    "url": "/static/js/2.7a5f568d.chunk.js"
+  },
+  {
+    "revision": "0387ace5873b0185fe89",
+    "url": "/static/js/main.24f09a92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "abafa1885068e3a5ddea2d6df6d71a5e",
+    "url": "/static/media/logo-darkT.abafa188.png"
+  },
+  {
+    "revision": "eb8ce9ddd83c03b4d6287ccdf9464fdf",
+    "url": "/static/media/logo-lightT.eb8ce9dd.png"
+  },
+  {
+    "revision": "4d16a0217efc34b22ff292bb6841f27e",
+    "url": "/static/media/profile.4d16a021.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81a9d8feda2934af14f2e6d245a0313d.js"
+  "/precache-manifest.0e8e39643624d7cffe9400917c19f02a.js"
 );
 
 self.addEventListener('message', (event) => {
