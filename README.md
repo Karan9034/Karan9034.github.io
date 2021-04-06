6110f8dc32aa08bde98df3ac623c3439a0826773
Karan9034 <karan.agr9034@gmail.com>
2021-04-06 16:41:49 +0530

    Resume Link Updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# [Portfolio](https://karan9034.github.io)
+# [Portfolio](https://karanel.tech)
 
 My portfolio website built using boilerplate template of [FdaviZ](https://github.com/kodi24fever/fdaviz)
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -14,7 +14,7 @@ const Intro = () => {
               <h1 className="intro-title mb-4">Hello, I am Karan Agrawal</h1>
               <p className="intro-subtitle"><span className="text-slider-items">
                   </span><strong className="text-slider"><Typed strings={['MERN Stack Developer', 'Flask Developer', 'ElectronJS Developer']} typeSpeed = {80} backDelay = {1100} backSpeed = {30} loop/></strong></p>
-              <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="https://drive.google.com/file/d/1-fNdqyJZybRARQxrGRLaljTvVJqhQtg4/view?usp=sharing" role="button">View My Resume</a></p>
+              <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="https://drive.google.com/file/d/1M4HFBdIyo-l5BqiRuJeBXPPNFmbnV4Px/view?usp=sharing" role="button">View My Resume</a></p>
             </div>
           </div>
         </div>
